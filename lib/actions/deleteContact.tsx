6648e0b7042edecf8be3c990eecf17693b900f3a
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-03-13 10:08:04 +0100

    Add missing revalidatepath

--- lib/actions/deleteContact.tsx
+++ lib/actions/deleteContact.tsx
@@ -1,5 +1,6 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import invariant from 'tiny-invariant';
 import { deleteContact as deleteContactFakeDb } from '../../data';
@@ -7,5 +8,6 @@ import { deleteContact as deleteContactFakeDb } from '../../data';
 export async function deleteContact(contactId: string) {
   invariant(contactId, 'Missing contactId param');
   await deleteContactFakeDb(contactId);
+  revalidatePath('/');
   redirect('/');
 }
