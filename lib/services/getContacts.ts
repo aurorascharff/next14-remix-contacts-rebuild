7e0a9dccde4876043680408e11eca99a89652ba4
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-04-04 21:47:57 +0200

    Simplify getContacts query

--- lib/services/getContacts.ts
+++ lib/services/getContacts.ts
@@ -1,12 +1,7 @@
 import 'server-only';
 
-import { notFound } from 'next/navigation';
 import { prisma } from '../../db';
 
 export async function getContacts() {
-  const contact = await prisma.contact.findMany();
-  if (!contact) {
-    notFound();
-  }
-  return contact;
+  return prisma.contact.findMany();
 }
