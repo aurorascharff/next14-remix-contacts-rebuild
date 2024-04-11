07bba607e497e8c3776b1d0f7aa9e0e4b58bd16c
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-04-11 19:26:20 +0200

    Use Remix's css for loading state

--- components/Details.tsx
+++ components/Details.tsx
@@ -6,5 +6,5 @@ import { cn } from '../utils/style';
 export const Details = ({ children }: { children: React.ReactNode }) => {
   const { isLoading } = useLoading();
 
-  return <div className={cn(isLoading && 'animate-pulse', 'w-full flex-1 px-16 py-8')}>{children}</div>;
+  return <div className={cn(isLoading && 'loading', 'w-full flex-1 px-16 py-8')}>{children}</div>;
 };
