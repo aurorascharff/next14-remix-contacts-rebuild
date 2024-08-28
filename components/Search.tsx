fc4a11c5d6556660516a9790863955d5e6753fdc
Aurora Scharff <aurora.sofie@gmail.com>
2024-08-28 14:57:25 +0200

    Update

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,27 +1,27 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import React, { useDeferredValue, useState } from 'react';
+import React, { useTransition } from 'react';
 import { cn } from '@/utils/style';
 
 export default function Search() {
   const router = useRouter();
   const pathName = usePathname();
   const searchParams = useSearchParams();
-  const [query, setQuery] = useState(searchParams.get('q') || '');
-  const deferredQuery = useDeferredValue(query);
-  const searching = query !== deferredQuery;
+  const query = searchParams.get('q') || '';
+  const [searching, startTransition] = useTransition();
 
   return (
     <form role="search">
       <input
         className={cn(!searching && 'search-icon', 'w-full pl-8 outline-offset-1')}
         onChange={e => {
-          setQuery(e.target.value);
-          const isFirstSearch = query === null;
-          isFirstSearch
-            ? router.push(`${pathName}?q=${e.target.value}`)
-            : router.replace(`${pathName}?q=${e.target.value}`);
+          startTransition(() => {
+            const isFirstSearch = query === null;
+            isFirstSearch
+              ? router.push(`${pathName}?q=${e.target.value}`)
+              : router.replace(`${pathName}?q=${e.target.value}`);
+          });
         }}
         defaultValue={query}
         aria-label="Search contacts"
